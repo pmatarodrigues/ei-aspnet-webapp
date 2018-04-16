26c2161d81e5eb31d202ed5f42be1e36056b041e
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2018-04-16 16:58:54 +0100

    Adicionar arquivos de projeto.

---
+++ MusicRepo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicRepo.Startup))]
+namespace MusicRepo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
